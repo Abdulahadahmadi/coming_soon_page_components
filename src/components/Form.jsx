530564e98136480bd023d1742282dff7b623cac8
= <=>
2022-06-15 11:07:22 +0430

    working on desktop responsive section

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,22 +3,36 @@ import React from 'react'
 function Form() {
     return (
         <main className='container flex flex-col px-8 justify-center items-center w-screen h-screen'>
-            <div className=''>
-                <div className=''>
-                    <h1 className='text-white font-bold text-3xl text-center mb-4 px-14'>Learn to code by watching others</h1>
-                    <p className='text-white text-md px-2 text-center'>See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.</p>
+            <div className='flex flex-col lg:flex-row'>
+
+                <div className='lg:mt-44 lg:px-44 lg:text-left'>
+                    <h1 className='text-white font-bold text-3xl text-center mb-4 px-14 lg:text-6xl'>Learn to code by watching others</h1>
+                    <p className='text-white text-md px-2 text-center lg:text-xl'>See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.</p>
                 </div>
-                    <div>
+
+
+                <div className=''>
+                    <div className=''>
                         <p className='text-white text-md py-3 px-20 my-4 mt-10 mx-auto items-center bg-purple-700 rounded-lg shadow-xl text-center'><span className='font-bold'>Try free 7 days</span> than $20/month thereafter</p>
                     </div>
                 <form className='bg-white px-10 py-8 rounded-lg shadow-xl text-black flex flex-col justify-center ' action='#'>
-                    <input className='border-2 py-2 px-8 rounded-lg my-2' type='text' placeholder='Firs Name' value='' />
-                    <input className='border-2 py-2 px-8 rounded-lg my-2' type='text' placeholder='Last Name' value='' />
-                    <input className='border-2 py-2 px-8 rounded-lg my-2' type='email' placeholder='Email Address' value='' />
-                    <input className='border-2 py-2 px-8 rounded-lg my-2' type='Password' placeholder='Password' value='' />
+                    <input className='border-2 py-2 px-8 rounded-lg my-2' type='text' placeholder='Firs Name'  />
+                    <input className='border-2 py-2 px-8 rounded-lg my-2' type='text' placeholder='Last Name'  />
+                    <input className='border-2 py-2 px-8 rounded-lg my-2' type='email' placeholder='Email Address' />
+                    <input className='border-2 py-2 px-8 rounded-lg my-2' type='Password' placeholder='Password' />
+                    <button className='text-white bg-green-500 uppercase px-10 py-2 my-2 text-sm hover:bg-green-900 rounded-lg shadow-xl'>Claim your free trail</button>
+                    <p className='text-center my-2'>by clicking the button you are agree to our<span className='text-red-500'> Terms and Conditions</span></p>
                 </form>
+                </div>
             </div>
+            <footer className='text-center w-screen text-white mt-5 lg:mt-20'>
+                <p class="attribution">
+                Challenge by <a href="#https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+                Coded by <a href="#ahad">Ahad</a>.
+                </p>
+        </footer>
         </main>
+        
     )
 }
 
